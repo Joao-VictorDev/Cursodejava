5998e015d96bb4e1d072bbaf598c46dbb342b050
Joao-VictorDev <joaovictor.dev.io@gmail.com>
2025-03-19 09:47:22 -0300

    Loops

---
+++ src/aula17/Main.java
@@ -0,0 +1,12 @@
+package aula17;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("For");
+
+        for (int j = 0; j < 11; j++){
+            System.out.print(j);
+        }
+
+    }
+}
