12af1ac82e5fcccc6cdf81801790d9c0b5904023
Joao-VictorDev <joaovictor.dev.io@gmail.com>
2025-03-19 15:34:09 -0300

    Matrizes

---
+++ src/aula20/MainP1.java
@@ -0,0 +1,19 @@
+package aula20;
+
+public class MainP1 {
+    public static void main(String[] args) {
+        // Matrizes (Arrays Multidimensionais) coluna = 10 linha = 5
+        double[][] notasAlunos = new double[30][4];
+
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 7;
+        notasAlunos[0][2] = 9;
+        notasAlunos[0][3] = 9.5;
+
+        notasAlunos[1][0] = 1.0;
+        notasAlunos[1][1] = 7.5;
+        notasAlunos[1][2] = 6.3;
+        notasAlunos[1][3] = 8.3;
+
+    }
+}
