9cb5b29a7fa29ce89efb2125a925c8979964a9b3
Joao-VictorDev <joaovictor.dev.io@gmail.com>
2025-03-25 09:33:47 -0300

    aula21

--- src/aula21/Main.java
+++ src/aula21/Main.java
@@ -1,7 +1,19 @@
 package aula21;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        int[] notas = new int[10];
 
+        Random random = new Random();
+
+        for (int i = 0; i < notas.length;i++){
+            notas[i] = random.nextInt(1,100);
+        }
+
+        for (int i : notas){
+            System.out.print(i + " ");
+        }
     }
 }
